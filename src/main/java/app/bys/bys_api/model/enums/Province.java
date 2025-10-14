fbe25afb5901758cd8c39574e2889f6a64120f19
Carlos <carlosf.munnoz@gmail.com>
2025-10-14 09:58:13 -0500

    Update: Change Province name and migration

--- src/main/java/app/bys/bys_api/model/enums/Province.java
+++ src/main/java/app/bys/bys_api/model/enums/Province.java
@@ -1,8 +1,9 @@
 package app.bys.bys_api.model.enums;
 
 public enum Province {
-    PROVINCE_NAME_1,
-    PROVINCE_NAME_2,
-    PROVINCE_NAME_3,
-    PROVINCE_NAME_4,
+    BARCELONA,
+    PUERTO_LA_CRUZ,
+    LECHERIA,
+    NUEVA_BARCELONA,
+    GUANTA
 }
