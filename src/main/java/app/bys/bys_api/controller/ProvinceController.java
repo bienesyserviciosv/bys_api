e98e4952706f6bfe33162992508f941be4a5f0eb
Carlos <carlosf.munnoz@gmail.com>
2025-10-15 14:58:50 -0500

    Refactor Province name

--- src/main/java/app/bys/bys_api/controller/ProvinceController.java
+++ src/main/java/app/bys/bys_api/controller/ProvinceController.java
@@ -1,6 +1,7 @@
 package app.bys.bys_api.controller;
 
 import app.bys.bys_api.model.enums.Province;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,10 @@ import java.util.List;
 public class ProvinceController {
 
     @GetMapping
-    public List<String> getProvinces() {
-        return Arrays.stream(Province.values()).map(Enum::name).toList();
+    public ResponseEntity<List<String>> getProvinces() {
+        List<String> provinceList = Arrays.stream(Province.values())
+                .map(Province::getDisplayName)
+                .toList();
+        return ResponseEntity.ok(provinceList);
     }
 }
--- src/main/java/app/bys/bys_api/model/enums/Province.java
+++ src/main/java/app/bys/bys_api/model/enums/Province.java
@@ -1,9 +1,20 @@
 package app.bys.bys_api.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+@Getter
 public enum Province {
-    BARCELONA,
-    PUERTO_LA_CRUZ,
-    LECHERIA,
-    NUEVA_BARCELONA,
-    GUANTA
+    BARCELONA("BARCELONA"),
+    PUERTO_LA_CRUZ("PUERTO LA CRUZ"),
+    LECHERIA("LECHERIA"),
+    NUEVA_BARCELONA("NUEVA BARCELONA"),
+    GUANTA("GUANTA");
+
+    @JsonValue
+    private final String displayName;
+
+    Province(String displayName) {
+        this.displayName = displayName;
+    }
 }
