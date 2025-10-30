77b7f7befa75bf5a40d908c8e6f9ad3ff4975160
Carlos <carlosf.munnoz@gmail.com>
2025-10-29 20:12:23 -0600

    Update: add prefix bys/ to pictures url

--- src/main/java/app/bys/bys_api/utils/MediaConstants.java
+++ src/main/java/app/bys/bys_api/utils/MediaConstants.java
@@ -2,9 +2,9 @@ package app.bys.bys_api.utils;
 
 public class MediaConstants {
 
-    public static final String PROVIDER_FOLDER = "provider/";
-    public static final String REQUEST_FOLDER = "request/";
-    public static final String USER_FOLDER = "user/";
-    public static final String PAYMENT_FOLDER = "payment/";
+    public static final String PROVIDER_FOLDER = "bys/provider/";
+    public static final String REQUEST_FOLDER = "bys/request/";
+    public static final String USER_FOLDER = "bys/user/";
+    public static final String PAYMENT_FOLDER = "bys/payment/";
 
 }
